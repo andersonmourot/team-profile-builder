963d067ca8ca109776dab7a05bbbb55d3bde4155
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2022-11-16 11:00:05 -0600

    Added templates for all 4 roles

---
+++ src/templates.js
@@ -0,0 +1,46 @@
+const engineerTemplate = engineer => {
+    const name = engineer.name;
+    const id = engineer.employee_id;
+    const email = engineer.email;
+    const role = engineer.getRole();
+    const github = engineer.getGitHub();
+    return `
+    <h2>${role.toUpperCase()}</h2>
+    <div>Name: ${name}</div>
+    <div>Email: ${email} </div>
+    <div>Employee ID: ${id}</div>
+    <div>GitHub: ${github}</div>
+    `
+}
+
+const managerTemplate = manager => {
+    const name = manager.name;
+    const id = manager.employee_id;
+    const email = manager.email;
+    const officeNumber = manager.getOfficeNumber();
+    const role = manager.getRole();
+    return `
+    <h2>${role.toUpperCase()}</h2>
+    <div>Name: ${name}</div>
+    <div>Email: ${email} </div>
+    <div>Employee ID: ${id}</div>
+    <div>Office Number: ${officeNumber}</div>
+    `
+}
+
+const internTemplate = intern => {
+    const name = intern.name;
+    const id = intern.employee_id;
+    const email = intern.email;
+    const role = intern.getRole();
+    const school = intern.getSchool();
+    return `
+    <h2>${role.toUpperCase()}</h2>
+    <div>Name: ${name}</div>
+    <div>Email: ${email} </div>
+    <div>Employee ID: ${id}</div>
+    <div>GitHub: ${school}</div>
+    `
+}
+
+module.exports = {engineerTemplate, managerTemplate, internTemplate};
